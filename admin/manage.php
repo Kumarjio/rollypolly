71170c38413f8fee336173f5994ab8829aebcced
Manish Khanchandani <naveenkhanchandani@gmail.com>
2015-04-05 14:22:17 -0700

    admin

---
+++ admin/manage.php
@@ -0,0 +1,28 @@
+<?php
+$pageTitle = 'Manage Cities, States, Country';
+?>
+<div style="width:100%">
+  <div style="float:left; text-align:center">
+    <a href="<?php echo HTTPPATH; ?>/admin/rules"><img src="<?php echo HTTPPATH; ?>/images/rules.png" /> <br />
+  Rules</a> </div>
+  <br style="clear:both;" />
+  <div style="float:left; text-align:center">
+    <a href="#"><img src="<?php echo HTTPPATH; ?>/images/plus.png" /> <br />
+  Add City</a> </div>
+  <div style="float:left; text-align:center">
+    <a href="#"><img src="<?php echo HTTPPATH; ?>/images/cities.png" /> <br />
+  My Cities</a> </div>
+  <div style="float:left; text-align:center">
+    <a href="#"><img src="<?php echo HTTPPATH; ?>/images/plus.png" /> <br />
+  Add State</a> </div>
+  <div style="float:left; text-align:center">
+    <a href="#"><img src="<?php echo HTTPPATH; ?>/images/cities.png" /> <br />
+  My States</a> </div>
+  <div style="float:left; text-align:center">
+    <a href="#"><img src="<?php echo HTTPPATH; ?>/images/plus.png" /> <br />
+  Add Country</a> </div>
+  <div style="float:left; text-align:center">
+    <a href="#"><img src="<?php echo HTTPPATH; ?>/images/cities.png" /> <br />
+  My Countries</a> </div>
+  <br style="clear:both;" />
+</div>
---
+++ admin/rules.php
@@ -0,0 +1,14 @@
+<?php
+$pageTitle = 'Rules to Own Cities, States, Country';
+?>
+  <div>
+    <p>You can apply to own any city, state, or country if it is not already owned by someone.</p>
+    <p>Each city, state, country's rate are different depending upon the popularity of that location. You need to bid the necessary amount to own the location. If your bid is highest among all the bids, then you will get opportunity to buy that location. If you fail to buy within 7 days of notice, it will be send to another second highest bidder to buy that location.</p>
+    <p>Once your buy a location, you can manage that location for a period of 1 year and new bidding will start again after 1 year. You have to pay your rate every month (subscription will last for 12 months from the day o</p>
+    <p>Earnings will come from advertisments, fees, subscriptions to some of the services in the website. Earnings will be distributed in the following manner:</p>
+    <p>1. City owner will get 20% of all the earnings from different sources pertaining to that location.</p>
+    <p>2. State owner will get 20% from each city in that state of all the earnings from different sources pertaining to that location.</p>
+    <p>3. Country owner will get 20% from each city in each state in that country of all the earnings from different sources pertaining to that location.</p>
+    <p>4. Rest all will be used for development and some benefits to the user.</p>
+    <p>&nbsp;</p>
+  </div>
